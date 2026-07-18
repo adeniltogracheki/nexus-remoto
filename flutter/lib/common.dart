8b1d4a41445d0e9b7dc450b1d39eb0a19eb68493
adeniltogracheki <adenilto.gracheki@waser.com.br>
2026-07-18 09:51:21 -0400

    Fix branding leaks (title bar, About dialog, translations, Android, Linux) + bump hbb_common submodule (use_ws fix)

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -3697,7 +3697,7 @@ Widget loadPowered(BuildContext context) {
     cursor: SystemMouseCursors.click,
     child: GestureDetector(
       onTap: () {
-        launchUrl(Uri.parse('https://rustdesk.com'));
+        launchUrl(Uri.parse('https://gmtec.tec.br'));
       },
       child: Opacity(
           opacity: 0.5,
